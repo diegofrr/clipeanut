4a1dc2fb699a8f3f272856a046ae730bbea10fea
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-12-25 23:41:58 -0300

    chore: next images config

--- yt-video-project/next.config.js
+++ yt-video-project/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['pipedproxy.kavin.rocks']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pipedproxy.kavin.rocks',
+                port: '',
+                pathname: '/**'
+            }
+        ]
     }
 };
 
