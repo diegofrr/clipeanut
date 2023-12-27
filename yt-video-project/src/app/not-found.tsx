f6b212b2236fbd1016f418fbf0a9eef85e56f2ed
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-12-27 18:17:38 -0300

    feat(wip): created not found page

--- yt-video-project/src/app/not-found.tsx
+++ yt-video-project/src/app/not-found.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
-    </div>
+    <main className="flex items-center flex-col gap-8 justify-center mt-16 min-h-screen-minus-navbar">
+      <h1 className="text-3xl text-white">Not found</h1>
+      <Link href="/">
+        <Button color="primary" radius="sm">
+          Go to Home
+        </Button>
+      </Link>
+    </main>
   );
 }
