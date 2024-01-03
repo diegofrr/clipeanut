44196ce6f739c79d15e559466a50555a30dd2006
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-03 01:16:08 -0300

    build: update build files

--- yt-video-project/next.config.js
+++ yt-video-project/next.config.js
@@ -1,15 +1,30 @@
+const instances = [
+    'kavin.rocks',
+    'syncpundit.io',
+    'lunar.icu',
+    'r4fo.com',
+    'projectsegfau.lt',
+    'privacydev.net',
+    'smnz.de',
+    'adminforge.de',
+    'astartes.nl',
+    'drgns.space',
+    'ggtyler.dev'
+];
+const remotePatterns = [];
+
+for (const instance of instances) {
+    remotePatterns.push({
+        protocol: 'https',
+        hostname: 'pipedproxy.' + instance,
+        port: '',
+        pathname: '/**'
+    });
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'pipedproxy.kavin.rocks',
-                port: '',
-                pathname: '/**'
-            }
-        ]
-    }
+    images: { remotePatterns }
 };
 
 module.exports = nextConfig;
