0cf21c4b978e0668b871a041ad772ce55c82c940
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-03 02:08:17 -0300

    feat: update settings page

--- yt-video-project/src/app/settings/page.tsx
+++ yt-video-project/src/app/settings/page.tsx
@@ -7,26 +7,42 @@ import Link from 'next/link';
 import { useContext } from 'react';
 
 export default function Settings() {
-  const { endpoint, instance, setInstance } = useContext(PipedInstanceContext);
+  const { endpoint, instance, setInstance, region, setRegion } = useContext(PipedInstanceContext);
 
-  const handleSelectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectionChangeInstance = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setInstance(e.target.value);
   };
 
+  const handleSelectionChangeRegion = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRegion(e.target.value);
+  };
+
   return (
-    <div className="w-full p-5">
-      <h1 className="text-white">{endpoint}</h1>
-      <h1 className="text-2xl text-white mb-5">Configurações</h1>
-      <div className="flex items-center justify-center flex-col">
+    <div className="w-full p-5 max-w-5xl m-auto">
+      <h1 className="text-2xl text-white mb-5 font-bold">Configurações</h1>
+      <div className="flex items-center justify-center flex-col gap-5">
         <Select
           className="w-full"
           label="Alterar instância"
           defaultSelectedKeys={[instance]}
-          onChange={handleSelectionChange}
+          onChange={handleSelectionChangeInstance}
         >
-          {PIPED_VALUES.INTANCES.map((instance) => (
-            <SelectItem key={instance} value={instance}>
-              {instance}
+          {PIPED_VALUES.INTANCES.map((_instance) => (
+            <SelectItem key={_instance} value={_instance} className={`${_instance === instance && 'hidden'}`}>
+              {_instance}
+            </SelectItem>
+          ))}
+        </Select>
+
+        <Select
+          className="w-full"
+          label="Alterar região"
+          defaultSelectedKeys={[region]}
+          onChange={handleSelectionChangeRegion}
+        >
+          {PIPED_VALUES.REGIONS.map((_region) => (
+            <SelectItem key={_region} value={_region} className={`${_region === region && 'hidden'}`}>
+              {_region}
             </SelectItem>
           ))}
         </Select>
