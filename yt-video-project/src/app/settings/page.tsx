c28b35eefff4dbbca63e0d5d1a00046c610fe186
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-03 01:15:57 -0300

    feat(wip): add settings page

---
+++ yt-video-project/src/app/settings/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { PIPED_VALUES } from '@/constants';
+import { PipedInstanceContext } from '@/contexts/pipedInstance';
+import { Select, SelectItem, Button } from '@nextui-org/react';
+import Link from 'next/link';
+import { useContext } from 'react';
+
+export default function Settings() {
+  const { endpoint, instance, setInstance } = useContext(PipedInstanceContext);
+
+  const handleSelectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setInstance(e.target.value);
+  };
+
+  return (
+    <div className="w-full p-5">
+      <h1 className="text-white">{endpoint}</h1>
+      <h1 className="text-2xl text-white mb-5">Configurações</h1>
+      <div className="flex items-center justify-center flex-col">
+        <Select
+          className="w-full"
+          label="Alterar instância"
+          defaultSelectedKeys={[instance]}
+          onChange={handleSelectionChange}
+        >
+          {PIPED_VALUES.INTANCES.map((instance) => (
+            <SelectItem key={instance} value={instance}>
+              {instance}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <Link href="/">
+        <Button color="primary" radius="sm" className="mt-5">
+          Go to Home
+        </Button>
+      </Link>
+    </div>
+  );
+}
