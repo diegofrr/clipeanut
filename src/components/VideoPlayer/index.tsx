ecfc14af69b7116cb9489907ee76da2112904115
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-07 16:16:17 -0300

    feat: implemented request auto retry method to get video data

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import { createRef, useCallback, useContext, useEffect } from 'react';
+import { createRef, useCallback, useContext, useEffect, useState } from 'react';
 
 import shaka from '@/lib/ShakaPlayer/shaka-player';
 
-import { DEFAULT_VALUES, PIPED_VALUES } from '@/constants';
-import { PipedInstanceContext } from '@/contexts/pipedInstance';
-import { generateDashFileFromFormats } from '@/utils/DashGenerator';
 import { FetchStreamOptionsType, fetchStream } from '@/services/actions/fetchStreamData';
+import { generateDashFileFromFormats } from '@/utils/DashGenerator';
+import { PipedInstanceContext } from '@/contexts/pipedInstance';
+import { DEFAULT_VALUES, PIPED_VALUES } from '@/constants';
+import { Button } from '@nextui-org/react';
+
+let boundGetVideoData = () => {};
 
 interface IVideoPlayerProps {
   videoId: string;
@@ -16,7 +19,9 @@ interface IVideoPlayerProps {
 }
 
 export default function VideoPlayer(props: IVideoPlayerProps) {
-  const { endpoint } = useContext(PipedInstanceContext);
+  const { endpoint, setInstance } = useContext(PipedInstanceContext);
+  const [canHandleRetry, setCanHandleRetry] = useState<boolean>(false);
+  const [pipedInstanceList, setPipedInstanceList] = useState<string[]>(PIPED_VALUES.INSTANCES);
 
   const videoRef = createRef<HTMLVideoElement>();
   const videoContainerRef = createRef<HTMLDivElement>();
@@ -27,7 +32,7 @@ export default function VideoPlayer(props: IVideoPlayerProps) {
     let mimeType = '';
     let uri = '';
 
-    const options = { videoId, endpoint, isFake: false, delay: 1 } as FetchStreamOptionsType;
+    const options = { videoId, endpoint, isFake: true, delay: 1 } as FetchStreamOptionsType;
 
     const stream = await fetchStream({ options });
 
@@ -46,6 +51,7 @@ export default function VideoPlayer(props: IVideoPlayerProps) {
           const url = new URL(stream.dash);
           url.searchParams.set('rewrite', 'false');
           uri = url.toString();
+          console.log('has dash');
         } else {
           const dash = await generateDashFileFromFormats(streamFormats, stream.duration);
           uri = PIPED_VALUES.VIDEO_TYPES.DASH_XML_DATA_URI + btoa(dash);
@@ -57,9 +63,7 @@ export default function VideoPlayer(props: IVideoPlayerProps) {
     return { mimeType, uri, stream };
   }, [endpoint, props]);
 
-  useEffect(() => {
-    if (!window?.document) return;
-
+  const loadPlayer = useCallback(async () => {
     const VIDEO_START_TIME = 0;
 
     const { onFailure, onSuccess } = props;
@@ -70,8 +74,7 @@ export default function VideoPlayer(props: IVideoPlayerProps) {
     ui.getControls();
 
     getVideoData()
-      .then((data) => {
-        const { mimeType, uri, stream } = data;
+      .then(({ mimeType, uri, stream }) => {
         player
           .load(uri, VIDEO_START_TIME, mimeType)
           .then(function () {
@@ -80,18 +83,37 @@ export default function VideoPlayer(props: IVideoPlayerProps) {
           })
           .catch(() => {
             onFailure && onFailure();
+            boundGetVideoData();
           });
       })
-      .catch(() => {
-        onFailure && onFailure();
-      });
-  }, [videoRef, videoContainerRef, props, endpoint, getVideoData]);
+      .catch(() => {});
+  }, [getVideoData, props, videoContainerRef, videoRef]);
+
+  boundGetVideoData = useCallback(() => {
+    if (pipedInstanceList.length > 0) {
+      setCanHandleRetry(false);
+      setInstance(pipedInstanceList[0]);
+      setPipedInstanceList(pipedInstanceList.slice(1));
+      console.log('Piped Instance:', pipedInstanceList);
+      loadPlayer();
+    } else setCanHandleRetry(true);
+  }, [loadPlayer, setInstance, pipedInstanceList]);
+
+  useEffect(() => {
+    if (window?.document) loadPlayer();
+  }, [loadPlayer]);
+
+  function handleRetryLoadStream() {
+    setPipedInstanceList(PIPED_VALUES.INSTANCES);
+    boundGetVideoData();
+  }
 
   return (
     <div>
       <div ref={videoContainerRef} className="mx-auto max-w-full w-[800px]">
         <video className="w-full h-full" ref={videoRef}></video>
       </div>
+      {canHandleRetry && <Button onClick={handleRetryLoadStream}>Tentar novamente</Button>}
     </div>
   );
 }
