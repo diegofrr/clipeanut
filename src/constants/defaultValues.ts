e4613128b21ac195ac06edb540a1d889e23c537a
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-07 16:12:51 -0300

    update minor issues

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,4 +1,5 @@
 export default {
   FAKE_REQUEST_DELAY: 2000,
+  MAX_REQUEST_TIMEOUT: 10000,
   FAKE_VIDEO_URI: '/assets/video.mp4'
 };
--- src/lib/ShakaPlayer/shaka-player.ts
+++ src/lib/ShakaPlayer/shaka-player.ts
@@ -1,5 +1,4 @@
 import shaka from 'shaka-player/dist/shaka-player.ui.js';
 import 'shaka-player/dist/controls.css';
-import './shaka-player.css';
 
 export default shaka;
