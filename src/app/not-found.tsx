b5690f2b6305b51f7a7f0f67bfc720012bf13a85
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-09 11:37:40 -0300

    style: minor updates

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <main className="flex items-center flex-col gap-8 justify-center mt-16 min-h-screen-minus-navbar">
+    <main className="flex items-center flex-col gap-8 justify-center mt-16 min-h-screen">
       <h1 className="text-3xl text-white">Not found</h1>
       <Button as={Link} href={'/'} color="primary" radius="sm">
         Go to Home
