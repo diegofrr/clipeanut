008f2bd0340115f091e1d1bb01128020b31d3360
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-09 12:32:34 -0300

    feat: add custom nextui-spinner

---
+++ src/app/components/TrendingVideo/skeleton.tsx
@@ -0,0 +1,3 @@
+export default function TrendingVideoSkeleton() {
+  return <div className="w-full h-80 bg-neutral-200 animate-pulse rounded-md" />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import { useCallback, useContext, useEffect, useState } from 'react';
 
-import NavBar from '@/components/Navbar';
+import CustomSpinner from '@/components/CustomSpinner';
 import ITrendingVideo from '@/types/TrendingVideo';
+import NavBar from '@/components/Navbar';
 
 import { FetchTrendingVideosOptionsType, fetchTrendingVideos } from '@/services/actions/fetchTrendingVideosData';
-import { TrendingVideo } from '@/app/components/TrendingVideo';
-import { Spinner } from '@nextui-org/react';
 import { PipedInstanceContext } from '@/contexts/pipedInstance';
+import { TrendingVideo } from '@/app/components/TrendingVideo';
 import { isFakeDataFetch } from '@/environments';
 
 export default function Home() {
@@ -19,7 +19,7 @@ export default function Home() {
 
   const handleGetTrendingVideos = useCallback(async () => {
     setLoading(true);
-    const options = { endpoint, region, isFake: isFakeDataFetch, delay: 1 } as FetchTrendingVideosOptionsType;
+    const options = { endpoint, region, isFake: isFakeDataFetch, delay: 100000000 } as FetchTrendingVideosOptionsType;
 
     await fetchTrendingVideos({ options })
       .then((data) => setTrendingVideos(data))
@@ -40,7 +40,7 @@ export default function Home() {
             <h1 className="text-3xl font-bold text-neutral-800 dark:text-neutral-200">Vídeos em alta</h1>
           </header>
 
-          {loading && <Spinner />}
+          {loading && <CustomSpinner size="lg" stroke="md" />}
           {!loading && trendingVideos.length > 0 && (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-center">
               {trendingVideos.map((video, index) => (
