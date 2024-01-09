b164da8cfbfc5da71f5479cfa4acce4bec945477
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-09 13:11:12 -0300

    feat(home/trending-videos): implement logic to retry fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,44 @@ import { PipedInstanceContext } from '@/contexts/pipedInstance';
 import { TrendingVideo } from '@/app/components/TrendingVideo';
 import { isFakeDataFetch } from '@/environments';
 
+import { HOME_PAGE_VALUES, PIPED_VALUES } from '@/constants';
+const { INITIAL_STATE } = HOME_PAGE_VALUES.TRENDING_VIDEO;
+
+let boundLoadTrendingVideos = () => {};
 export default function Home() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [trendingVideos, setTrendingVideos] = useState<ITrendingVideo[]>([]);
+  const { endpoint, region, setInstance, instance } = useContext(PipedInstanceContext);
 
-  const { endpoint, region, instance } = useContext(PipedInstanceContext);
+  const [loading, setLoading] = useState<boolean>(INITIAL_STATE.LOADING);
+  const [trendingVideos, setTrendingVideos] = useState<ITrendingVideo[]>(INITIAL_STATE.TRENDING_VIDEOS);
+  const [pipedInstanceList, setPipedInstanceList] = useState<string[]>(INITIAL_STATE.pipedInstanceList);
 
-  const handleGetTrendingVideos = useCallback(async () => {
+  const loadTrendingVideos = useCallback(async () => {
     setLoading(true);
     const options = { endpoint, region, isFake: isFakeDataFetch, delay: 1 } as FetchTrendingVideosOptionsType;
 
     await fetchTrendingVideos({ options })
-      .then((data) => setTrendingVideos(data))
-      .catch(() => {})
-      .finally(() => setLoading(false));
-  }, [endpoint, region]);
+      .then((data) => {
+        setTrendingVideos(data);
+        setLoading(false);
+        setPipedInstanceList(PIPED_VALUES.INSTANCES);
+        setInstance(instance);
+      })
+      .catch(() => {
+        boundLoadTrendingVideos();
+      });
+  }, [endpoint, region, setInstance, instance]);
+
+  boundLoadTrendingVideos = useCallback(() => {
+    if (!pipedInstanceList.length) return;
+    setInstance(pipedInstanceList[0]);
+    setPipedInstanceList(pipedInstanceList.slice(1));
+    loadTrendingVideos();
+  }, [loadTrendingVideos, pipedInstanceList, setInstance]);
 
   useEffect(() => {
-    handleGetTrendingVideos();
-  }, [instance, handleGetTrendingVideos]);
+    if (window?.document) loadTrendingVideos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
