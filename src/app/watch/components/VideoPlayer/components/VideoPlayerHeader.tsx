19a19d7c8125ea362e1146582111202df79b6ffb
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-10 12:16:18 -0300

    refactor: change image component (next/img => nextui/img)

--- src/app/watch/components/VideoPlayer/components/VideoPlayerHeader.tsx
+++ src/app/watch/components/VideoPlayer/components/VideoPlayerHeader.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import { useContext } from 'react';
+import { Image } from '@nextui-org/react';
 import { StreamContext } from '../contexts/stream';
 import { formatters } from '@/utils';
 
@@ -11,7 +12,15 @@ export function VideoPlayerHeader() {
     <header className="flex items-center gap-4 text-white flex-row">
       {streamUploader.uploader && (
         <>
-          <img src={streamUploader.uploaderAvatar} alt="" className="w-10 h-10 rounded-full font-bold" />
+          <div className="bg-default-200 relative min-w-[40px] min-h-[40px] w-10 h-10 rounded-full overflow-hidden">
+            <Image
+              src={streamUploader.uploaderAvatar}
+              alt={streamUploader.uploader + ' avatar'}
+              width={40}
+              height={40}
+              className="h-10 w-10 min-w-10 min-h-10"
+            />
+          </div>
           <div className="flex items-start justify-center flex-col">
             <h1>{streamUploader.uploader}</h1>
             <span className="text-xs text-gray-400">
