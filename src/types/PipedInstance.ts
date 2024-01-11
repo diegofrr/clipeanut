6702e7d11d0fe202b31db36371a9cb8fd2c5727a
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-11 06:03:05 -0300

    feat: add piped instances interface + mock

--- src/types/PipedInstance.ts
+++ src/types/PipedInstance.ts
@@ -1,4 +1,17 @@
 export default interface IPipedInstance {
-  endpoint: string;
-  region: string;
+  name: string;
+  api_url: string;
+  locations: string;
+  version: string;
+  up_to_date: boolean;
+  cdn: boolean;
+  registered: number;
+  last_checked: number;
+  cache: boolean;
+  s3_enabled: boolean;
+  image_proxy_url: string;
+  registration_disabled: boolean;
+  uptime_24h: number;
+  uptime_7d: number;
+  uptime_30d: number;
 }
