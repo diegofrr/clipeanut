9e8559010af39e6c3c092bae2c360147676015cf
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-11 23:19:26 -0300

    feat: create useLocalStorageWithExpiration hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+import { useLocalStorageWithExpiration } from './useLocalStorageWithExpiration';
+
+export { useLocalStorageWithExpiration };
---
+++ src/hooks/useLocalStorageWithExpiration.ts
@@ -0,0 +1,64 @@
+interface IExpireOptions {
+  seconds?: number;
+  minutes?: number;
+  days?: number;
+}
+
+interface IStoragedItem<T> {
+  value: T;
+  expirationTime?: number;
+}
+
+type UseLocalStoragedWithExpiration<T> = {
+  getStoragedItem: (key: string) => IStoragedItem<T> | null;
+  setStoragedItem: (key: string, value: T, expireOptions: IExpireOptions) => void;
+  removeStoragedItem: (key: string) => void;
+  isExpiredItem: (key: string) => boolean;
+  isExistsItem: (key: string) => boolean;
+};
+
+export function useLocalStorageWithExpiration<T>(): UseLocalStoragedWithExpiration<T> {
+  const getStoragedItem = (key: string): IStoragedItem<T> | null => {
+    const storagedItem = JSON.parse(localStorage.getItem(key) || 'null') as IStoragedItem<T>;
+
+    if (!storagedItem) return null;
+    else if (!storagedItem.expirationTime || !isExpiredTime(storagedItem.expirationTime)) return storagedItem;
+    else {
+      removeStoragedItem(key);
+      return null;
+    }
+  };
+
+  const setStoragedItem = (key: string, value: T, expireOptions: IExpireOptions) => {
+    const { seconds, minutes, days } = expireOptions;
+    let expirationTime;
+
+    if (seconds) expirationTime = new Date().getTime() + seconds * 1000;
+    else if (minutes) expirationTime = new Date().getTime() + minutes * 60 * 1000;
+    else if (days) expirationTime = new Date().getTime() + days * 24 * 60 * 60 * 1000;
+
+    localStorage.setItem(key, JSON.stringify({ value, expirationTime }));
+  };
+
+  const removeStoragedItem = (key: string) => {
+    localStorage.removeItem(key);
+  };
+
+  const isExpiredItem = (key: string): boolean => {
+    const storagedValue = getStoragedItem(key);
+
+    if (!storagedValue) return false;
+    else if (!storagedValue.expirationTime) return false;
+    else return isExpiredTime(storagedValue.expirationTime);
+  };
+
+  const isExistsItem = (key: string): boolean => {
+    return !!localStorage.getItem(key);
+  };
+
+  const isExpiredTime = (expirationTime: number): boolean => {
+    return new Date().getTime() > expirationTime;
+  };
+
+  return { getStoragedItem, setStoragedItem, removeStoragedItem, isExpiredItem, isExistsItem };
+}
