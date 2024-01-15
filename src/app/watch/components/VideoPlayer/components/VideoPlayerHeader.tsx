6c5e9e45a00397536c371db716d359490bcbd68a
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-15 10:29:26 -0300

    feat: add function to load avatar image from piped instance on error

--- src/app/watch/components/VideoPlayer/components/VideoPlayerHeader.tsx
+++ src/app/watch/components/VideoPlayer/components/VideoPlayerHeader.tsx
@@ -1,31 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
-import { useContext } from 'react';
-import { Image } from '@nextui-org/react';
+import { createRef, useContext } from 'react';
+import { Avatar } from '@nextui-org/react';
 
 import { StreamContext } from '../contexts/stream';
 import { StreamUtils } from '@/utils';
 
 export function VideoPlayerHeader() {
   const { streamUploader, stream } = useContext(StreamContext);
+  const avatarRef = createRef<HTMLSpanElement>();
+
+  function onLoadAvatarError() {
+    const img = avatarRef?.current?.querySelector('img') as HTMLImageElement;
+    img.src = streamUploader.uploaderAvatar;
+    img.style.opacity = '1';
+  }
 
   return (
-    <header className="flex items-center gap-4 text-white flex-row">
+    <header className="flex items-center gap-4 flex-row">
       {streamUploader.uploader && (
         <>
           <div className="bg-default-200 relative min-w-[40px] min-h-[40px] w-10 h-10 rounded-full overflow-hidden">
-            <Image
+            <Avatar
+              ref={avatarRef}
+              name={streamUploader.uploader}
               src={StreamUtils.channelImagemUrlGenerator(streamUploader.uploaderAvatar)}
-              alt={streamUploader.uploader + ' avatar'}
-              width={40}
-              height={40}
-              className="h-10 w-10 min-w-10 min-h-10"
+              onError={onLoadAvatarError}
             />
           </div>
           <div className="flex items-start justify-center flex-col">
             <h1>{streamUploader.uploader}</h1>
-            <span className="text-xs text-gray-400">
-              {StreamUtils.formatStreamStats(streamUploader.uploaderSubscriberCount)}
-            </span>
+            <span className="text-xs">{StreamUtils.formatStreamStats(streamUploader.uploaderSubscriberCount)}</span>
           </div>
           {streamUploader.uploaderVerified && <span>✅</span>}
           <span>👀 {StreamUtils.formatStreamStats(stream.views)}</span>
