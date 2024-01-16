0aa23c5b57c590e62ba0586bed73302582d55371
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-16 01:49:04 -0300

    feat: add custom keyboard actions

--- src/app/watch/components/VideoPlayer/components/VideoPlayerContent.tsx
+++ src/app/watch/components/VideoPlayer/components/VideoPlayerContent.tsx
@@ -106,7 +106,8 @@ export default function VideoPlayerContent() {
 
       setIsVideoLoaded(true);
       removeSubtitleOption();
-      customUiController(ui.$controllerBottom);
+      addCustomUiController(ui.$controllerBottom);
+      addCustomKeyboardActions(player);
 
       if (stream.livestream) {
         ui.$coverButton.onclick = () => {
@@ -115,12 +116,33 @@ export default function VideoPlayerContent() {
       }
     }
 
+    function addCustomKeyboardActions(player: Player) {
+      const seekTime = player.duration / 9;
+
+      document.addEventListener('keydown', ({ code }) => {
+        if (code === 'Space') player.togglePlay();
+        else if (code === 'm') player.toggleMute();
+        else if (code === 'ArrowRight') player.seek(player.currentTime + 5);
+        else if (code === 'ArrowLeft') player.seek(player.currentTime - 5);
+        else if (code === 'Digit0' || code === 'Numpad0') player.seek(0);
+        else if (code === 'Digit1' || code === 'Numpad1') player.seek(seekTime * 1);
+        else if (code === 'Digit2' || code === 'Numpad2') player.seek(seekTime * 2);
+        else if (code === 'Digit3' || code === 'Numpad3') player.seek(seekTime * 3);
+        else if (code === 'Digit4' || code === 'Numpad4') player.seek(seekTime * 4);
+        else if (code === 'Digit5' || code === 'Numpad5') player.seek(seekTime * 5);
+        else if (code === 'Digit6' || code === 'Numpad6') player.seek(seekTime * 6);
+        else if (code === 'Digit7' || code === 'Numpad7') player.seek(seekTime * 7);
+        else if (code === 'Digit8' || code === 'Numpad8') player.seek(seekTime * 8);
+        else if (code === 'Digit9' || code === 'Numpad9') player.seek(seekTime * 8.8);
+      });
+    }
+
     function removeSubtitleOption() {
       if (hasMoreThanOneSubtitle) return;
       document.querySelector('[data-key="oplayer-plugin-dash-Language"]')?.remove();
     }
 
-    function customUiController(controller: HTMLElement) {
+    function addCustomUiController(controller: HTMLElement) {
       controller.querySelectorAll('button').forEach((button) => {
         button.classList.add('custom-tooltip');
         if (button.getAttribute('aria-label') === 'Volume') {
