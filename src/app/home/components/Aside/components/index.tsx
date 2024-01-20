16faec4cd09d43eca0023c6c10347e7816ba029c
Diêgo Ferreira <diego.raian@lavid.ufpb.br>
2024-01-20 02:25:51 -0300

    chore: remove <aside> component

--- src/app/home/components/Aside/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Musics from './Musics';
-
-export default function Aside() {
-  return (
-    <aside className="mx-6 sm:mx-0 h-fit min-w-0 sm:max-w-full md:sticky md:min-w-[200px] lg:min-w-[240px] md:top-[calc(65px+1.5rem)]">
-      <Musics />
-    </aside>
-  );
-}
