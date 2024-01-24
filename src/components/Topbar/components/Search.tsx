cd100262f4e4d07286949a23b6dc33b2dc2637c1
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-24 15:39:01 -0300

    style(topbar): search input border

--- src/components/Topbar/components/Search.tsx
+++ src/components/Topbar/components/Search.tsx
@@ -26,7 +26,8 @@ export default function Search() {
         base: 'sm:max-w-md lg:max-w-lg w-full h-10',
         mainWrapper: 'h-full',
         input: 'text-small',
-        inputWrapper: 'h-full font-normal text-default-500 bg-neutral-100 dark:bg-neutral-900'
+        inputWrapper:
+          'h-full font-normal text-default-500 border-1 bg-neutral-100 dark:bg-neutral-900 border-1 border-neutral-200 dark:border-none'
       }}
       radius="full"
       type="search"
