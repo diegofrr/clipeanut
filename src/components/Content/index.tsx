7c56d565e039d040e2309e6db163690379114b0c
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-01-25 09:42:04 -0300

    style(content-component): change padding-bottom

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -10,9 +10,10 @@ export default function Content({ children, ...props }: ContentProps) {
   return (
     <div
       {...props}
-      className={
-        `${isHome ? 'px-0 sm:px-6 md:px-0 md:pr-6' : 'pl-6 md:pl-0 pr-6'} lg:pr-12 py-6 mt-16 ` + props.className || ''
-      }
+      className={`lg:pr-12 pt-6 pb-24 md:pb-6 mt-16
+      ${isHome ? 'px-0 sm:px-6 md:px-0 md:pr-6' : 'pl-6 md:pl-0 pr-6'} 
+      ${props.className || ''}
+      `}
     >
       {children}
     </div>
