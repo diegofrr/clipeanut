90f5b2d8ce99d19873f767ceeecab5d8a4f62df2
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-02-03 20:51:40 -0300

    refactor: move toggle theme button to topbar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,40 +1,18 @@
 'use client';
 
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
 import Icons from '@/icons';
 
-import type { UserTheme } from './types';
-
-import { Button, Tooltip } from '@nextui-org/react';
+import { Tooltip } from '@nextui-org/react';
 import { useWindowSize } from 'usehooks-ts';
-import { useTheme } from 'next-themes';
 import { hasSidebar } from './utils';
 import { links } from './links';
 
 export default function Sidebar() {
   const pathname = usePathname();
   const { width } = useWindowSize();
-
-  const { setTheme, resolvedTheme } = useTheme();
-
-  const [isClient, setIsClient] = useState(false);
-  const [userTheme, setUserTheme] = useState<UserTheme>({ name: '', icon: <></> });
-
-  useEffect(() => setIsClient(true), []);
-
-  useEffect(() => {
-    setUserTheme({
-      name: resolvedTheme === 'light' ? 'claro' : 'escuro',
-      icon: resolvedTheme === 'light' ? <Icons.Sun /> : <Icons.Moon />
-    });
-  }, [resolvedTheme]);
-
-  function handleToggleTheme() {
-    setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
-  }
 
   return hasSidebar(pathname) ? (
     <nav className="bottom-0 w-full h-16 md:h-auto md:w-20 lg:w-32 fixed left-0 md:top-16 bg-white dark:bg-black p-0 md:py-6 z-50">
@@ -56,19 +34,6 @@ export default function Sidebar() {
               </li>
             </Tooltip>
           ))}
-
-          {isClient && width >= 768 && (
-            <Tooltip content={userTheme.name}>
-              <Button
-                className="absolute bottom-0"
-                radius="full"
-                onClick={handleToggleTheme}
-                startContent={userTheme.icon}
-                isIconOnly
-                variant="light"
-              />
-            </Tooltip>
-          )}
         </ul>
       </div>
     </nav>
--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,28 +1,47 @@
 'use client';
 
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
+import Icons from '@/icons';
 import Search from './components/Search';
 import ReactCountryFlag from 'react-country-flag';
 
+import type { UserTheme } from './types';
 import { AppBanner, AppLogo } from '../../../public/assets';
 import { CommonUtils } from '@/utils';
 import { hasTopbar, hasSearchInput } from './utils';
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from '@nextui-org/react';
 
-import { PIPED_VALUES } from '@/constants';
 import { PipedInstanceContext } from '@/contexts/pipedInstance';
 import { useWindowSize } from 'usehooks-ts';
+import { useTheme } from 'next-themes';
+
+import { PIPED_VALUES } from '@/constants';
 
 export default function Topbar() {
   const pathname = usePathname();
 
   const { region, setRegion } = useContext(PipedInstanceContext);
+  const { setTheme, resolvedTheme } = useTheme();
   const { width } = useWindowSize();
 
+  const [isClient, setIsClient] = useState(false);
+  const [userTheme, setUserTheme] = useState<UserTheme>({ name: '', icon: <></> });
+
+  useEffect(() => setIsClient(true), []);
+
+  useEffect(() => {
+    setUserTheme({
+      name: resolvedTheme === 'light' ? 'claro' : 'escuro',
+      icon: resolvedTheme === 'light' ? <Icons.Sun /> : <Icons.Moon />
+    });
+  }, [resolvedTheme]);
+
   const isMobile = () => width <= 640;
+
+  const handleToggleTheme = () => setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
 
   return hasTopbar(pathname) ? (
     <header className="fixed top-0 left-0 px-6 lg:px-12 right-0 max-h-16 h-16 z-max bg-background">
@@ -34,35 +53,48 @@ export default function Topbar() {
 
         {hasSearchInput(pathname) && <Search />}
 
-        <Dropdown backdrop="opaque" style={{ zIndex: 999999999 }}>
-          <DropdownTrigger>
+        <div className="flex flex-row">
+          {isClient && (
             <Button
-              isIconOnly={isMobile()}
-              variant="light"
+              className="hover:text-app_orange-600"
               radius="full"
-              className="font-bold"
-              startContent={<ReactCountryFlag countryCode={region} style={{ fontSize: 20 }} />}
-            >
-              {!isMobile() && region}
-            </Button>
-          </DropdownTrigger>
+              onClick={handleToggleTheme}
+              startContent={userTheme.icon}
+              isIconOnly
+              variant="light"
+            />
+          )}
 
-          <DropdownMenu aria-label="Countries" className="max-h-[86vh] mt-3 overflow-auto overflow-x-hidden">
-            <DropdownSection title="Região do conteúdo">
-              {PIPED_VALUES.REGIONS.map((country) => (
-                <DropdownItem
-                  onClick={() => setRegion(country)}
-                  key={country}
-                  variant="light"
-                  className="font-bold rounded-full hover:bg-foreground-100"
-                  startContent={<ReactCountryFlag countryCode={country} style={{ fontSize: 20 }} />}
-                >
-                  {CommonUtils.getCountryName(country)}
-                </DropdownItem>
-              ))}
-            </DropdownSection>
-          </DropdownMenu>
-        </Dropdown>
+          <Dropdown backdrop="opaque" style={{ zIndex: 999999999 }}>
+            <DropdownTrigger>
+              <Button
+                isIconOnly={isMobile()}
+                variant="light"
+                radius="full"
+                className="font-bold"
+                startContent={<ReactCountryFlag countryCode={region} style={{ fontSize: 20 }} />}
+              >
+                {!isMobile() && region}
+              </Button>
+            </DropdownTrigger>
+
+            <DropdownMenu aria-label="Countries" className="max-h-[86vh] mt-3 overflow-auto overflow-x-hidden">
+              <DropdownSection title="Região do conteúdo">
+                {PIPED_VALUES.REGIONS.map((country) => (
+                  <DropdownItem
+                    onClick={() => setRegion(country)}
+                    key={country}
+                    variant="light"
+                    className="font-bold rounded-full hover:bg-foreground-100"
+                    startContent={<ReactCountryFlag countryCode={country} style={{ fontSize: 20 }} />}
+                  >
+                    {CommonUtils.getCountryName(country)}
+                  </DropdownItem>
+                ))}
+              </DropdownSection>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
       </div>
     </header>
   ) : (
