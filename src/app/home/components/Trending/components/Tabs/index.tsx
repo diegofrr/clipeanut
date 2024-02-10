0b952a24ab208785385284821acc7081bb1590e2
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-02-10 13:39:37 -0300

    refactor: move selected region to the top

--- src/app/home/components/Trending/components/Tabs/index.tsx
+++ src/app/home/components/Trending/components/Tabs/index.tsx
@@ -81,14 +81,15 @@ export default function TrendingTabs({ tab, setTab, ...props }: TypeTabsProps) {
           >
             {PIPED_VALUES.REGIONS.map((country) => (
               <SelectItem
+                isDisabled={country === region}
                 key={country}
                 variant="light"
-                className="font-bold rounded-full hover:bg-foreground-100"
+                className={`font-bold rounded-full hover:bg-foreground-100`}
                 startContent={<ReactCountryFlag countryCode={country} style={{ fontSize: 20 }} />}
               >
                 {CommonUtils.getCountryName(country)}
               </SelectItem>
-            ))}
+            )).sort((a) => (a.props.startContent.props.countryCode === region ? -1 : 1))}
           </Select>
           <Button
             isDisabled={isSelectOpen}
