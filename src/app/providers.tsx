839aa266c34872f38ed72ea16b90a814f230db31
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-02-16 00:19:19 -0300

    refactor: removed unused context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,6 @@
 
 import { useRouter } from 'next/navigation';
 
-import PipedInstanceProvider from '@/contexts/pipedInstance';
-
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { NextUIProvider } from '@nextui-org/react';
 import { Toaster } from 'react-hot-toast';
@@ -12,13 +10,11 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
-    <PipedInstanceProvider>
-      <NextThemesProvider attribute="class" defaultTheme="system">
-        <NextUIProvider navigate={router.push}>
-          <Toaster />
-          {children}
-        </NextUIProvider>
-      </NextThemesProvider>
-    </PipedInstanceProvider>
+    <NextThemesProvider attribute="class" defaultTheme="system">
+      <NextUIProvider navigate={router.push}>
+        <Toaster />
+        {children}
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
